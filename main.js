5835f2226be0315ad7493da6b15dffcfcc5fe478
LucaAgatinoPrivitera <priviteraluca01@gmail.com>
2024-05-03 15:18:00 +0200

    Milestone 1 - fatta

---
+++ main.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function generaNumeriSenzaDuplicati() {
+    let numeri = [];
+    while (numeri.length < 5) {
+        let nuovoNumero = Math.floor(Math.random() * 101); // Genera un numero casuale da 1 a 100
+        if (!numeri.includes(nuovoNumero)){ // Verifica se il numero non è già presente nell'array
+            numeri.push(nuovoNumero); // Aggiunge il numero all'array se non è duplicato
+        }
+    }
+    return numeri;
+}
