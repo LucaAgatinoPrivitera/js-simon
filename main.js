497a42dfed6455cb17f29f6d86a352e158ab4eba
LucaAgatinoPrivitera <priviteraluca01@gmail.com>
2024-05-03 15:55:34 +0200

    Milestone 3 - Fatta

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
 let testoDaModificare = document.querySelector("h1");
 let btn = document.querySelector("button");
+let numeri = [];
+let userNumbers = [];
+
+
+btn.addEventListener("click", cancellaTesto()); //problema col click, lo script pensa che clicco ad ogni avvio della pagina
+
+setTimeout(cancellaTesto, 5 * 1000); //funziona ma ho sempre quel problema col click
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -13,19 +31,25 @@ let btn = document.querySelector("button");
 
 
 function generaNumeriSenzaDuplicati() {
-    let numeri = [];
-    testoDaModificare.innerHTML ="";
+    testoDaModificare.innerHTML = "";
     while (numeri.length < 5) {
         let nuovoNumero = Math.floor(Math.random() * 101); // Genera un numero casuale da 1 a 100
-        if (!numeri.includes(nuovoNumero)){ // Verifica se il numero non è già presente nell'array
+        if (!numeri.includes(nuovoNumero)) { // Verifica se il numero non è già presente nell'array
             numeri.push(nuovoNumero); // Aggiunge il numero all'array se non è duplicato
             testoDaModificare.innerHTML = numeri; //da chiedere come posso fare in modo da gestire lo spazio ad ognuno perché adesso li stampa e basta
         }
     }
-    
     return numeri;
 }
 
 
+function cancellaTesto() {
+    testoDaModificare.classList.add("stealth"); //Mi mette opacity a 0
+}
 
-btn.addEventListener("click", generaNumeriSenzaDuplicati ());
+function inserisciNumeri() {
+    while (userNumbers.length <= numeri.length-1) {
+        userSingleNumber = prompt("scrivi i tuoi numeri");
+        userNumbers.push(userSingleNumber);
+    }
+}
